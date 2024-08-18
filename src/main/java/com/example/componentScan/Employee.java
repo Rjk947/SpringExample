30549c2bdfacbf7e1ff7f6250c4810fa6e9c9f76
Rajesh Kumar <rajesh8507kumar@gmail.com>
2024-08-18 13:13:57 +0530

    autowire

---
+++ src/main/java/com/example/componentScan/Employee.java
@@ -0,0 +1,62 @@
+package com.example.componentScan;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+
+    @Value("123")
+    private int id;
+
+    @Value("${Java.Home}")
+    private String name;
+
+    @Value("#{100*100}")
+    private double salary;
+
+    @Value("Software Engineer")
+    private String designation;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                ", designation='" + designation + '\'' +
+                '}';
+    }
+}
